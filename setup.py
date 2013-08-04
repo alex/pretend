1b7c13a2769656ddf4d1d09d65755724cc5e2880
Alex Gaynor <alex.gaynor@gmail.com>
2013-08-04 11:38:52 -0700

    Bumped version number

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.rst") as f:
     readme = f.read()
 
 setup(
-    version="1.0.2",
+    version="1.0.3",
     name="pretend",
     description="A library for stubbing in Python",
     long_description=readme,
