423a4d99137c81a2af1cce4a9d4732fde1b0e4d5
Alex Gaynor <alex.gaynor@gmail.com>
2018-05-27 14:13:54 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -22,9 +22,8 @@ setup(
         "Topic :: Software Development :: Testing",
         "Programming Language :: Python :: 2.6",
         "Programming Language :: Python :: 2.7",
-        "Programming Language :: Python :: 3.2",
-        "Programming Language :: Python :: 3.3",
         "Programming Language :: Python :: 3.4",
         "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
     ]
 )
