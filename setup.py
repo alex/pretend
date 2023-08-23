0d9a41cfca8becbbc6dca158f40a4c2ad6831aea
Alex Gaynor <alex.gaynor@gmail.com>
2023-08-23 17:51:56 -0400

    Refresh CI

--- setup.py
+++ setup.py
@@ -20,11 +20,10 @@ setup(
         "License :: OSI Approved :: BSD License",
         "Operating System :: OS Independent",
         "Topic :: Software Development :: Testing",
-        "Programming Language :: Python :: 2.7",
-        "Programming Language :: Python :: 3.5",
-        "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
     ]
 )
